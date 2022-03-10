402c33002014aaa8ab71187573953730b081b817
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-10 13:05:15 +0300

    last

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
+ * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}
