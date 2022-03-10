83a30dd146f39bda0c5adb7b982adece52e1cec2
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-10 12:17:45 +0300

    last

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
