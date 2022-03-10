0b48bb2b711b5eafcb539e8b34ff0413e367020d
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-10 12:56:47 +0300

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+return (1);
+}
+
