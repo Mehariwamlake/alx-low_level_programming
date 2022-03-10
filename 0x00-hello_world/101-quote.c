51a4296da89be6c0143d68dfc3eee60fed9bd2a8
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-10 12:58:38 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
-
