7c895bc695b273010990d919001ff61192f0f657
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-11 18:14:37 +0300

    last

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic
+ * main - Prints the alphabetic in lower and upper case
  *
  * Return: Always (Success)
  */
@@ -10,8 +10,16 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
 
 	putchar('\n');
+
 	return (0);
 }
