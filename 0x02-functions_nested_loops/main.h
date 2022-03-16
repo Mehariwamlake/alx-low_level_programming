0eeb80df88339277790ae3c3a81162a1b34ed8a9
mehariwamlake <mehariwamlkae@gmail.com>
2022-03-16 19:33:41 +0300

    last

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,4 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endifi
+#endif
