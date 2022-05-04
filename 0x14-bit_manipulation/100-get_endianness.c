389bc35c34dc3e6502449dfcc796db6a75bb3e2e
mehariw amlake <you@example.com>
2022-05-04 02:48:19 -0700

    last

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ *
+ * Return: void function
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+
+
+	if (*(char *) &n == 1)
+		return (1);
+	return (0);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,6 @@
+# C = Bit Manipulation 
+
+Editors used: `vi, vim, emacs`
+All files compiled on Ubuntu 20.04 LTS using `gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89`
+
+## Prototypes
